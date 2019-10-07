bb70471ad081cd9e30dbea29f8fe9fc6c4c5168a
Alagappan Maruthappan <aswin@aswin.local>
2019-10-06 17:25:33 -0700

    modularized run file

--- python/DeepRTS/run.py
+++ python/DeepRTS/run.py
@@ -3,56 +3,82 @@ import pygame
 
 from DeepRTS import PyDeepRTS
 
-# Start the game
-g = PyDeepRTS('10x10-2v2.json')
+MAP_NAME = '10x10-2v2.json'
+AUTO_ATTACK = True
+AUTO_HARVEST = True
+NUM_OF_GAMES = 3
 
-# Add players - By default 2 players gets added
-# player1 = g.add_player()
-# player2 = g.add_player()
-player1 = g.players[0]
-player2 = g.players[1]
 
-print("Name : " + player1.get_name())
-print("Name : " + player2.get_name())
+def setup_game():
+    g = PyDeepRTS(MAP_NAME)
 
-# Set FPS and UPS limits
-g.set_max_fps(10000000)
-g.set_max_ups(10000000)
+    # Set FPS and UPS limits
+    g.set_max_fps(10000000)
+    g.set_max_ups(10000000)
 
-# How often the state should be drawn
-g.render_every(1)
+    # How often the state should be drawn
+    g.render_every(1)
 
-# How often the capture function should return a state
-g.capture_every(1)
+    # How often the capture function should return a state
+    g.capture_every(1)
 
-# How often the game image should be drawn to the screen
-g.view_every(1)
+    # How often the game image should be drawn to the screen
+    g.view_every(1)
 
-# Start the game (flag)
-g.start()
+    # Configuration
+    g.config.set_auto_attack(AUTO_ATTACK)
+    g.config.set_harvest_forever(AUTO_HARVEST)
 
-# Run forever
-while True:
-    g.tick()  # Update the game clock
-    g.update()  # Process the game state
-    g.render()  # Draw the game state to graphics
-    # Captures current state (Returns None if .capture_every is set for some iterations)
-    state = g.capture()
-    g.caption()  # Show Window caption
+    return g
 
-    g.view()  # View the game state in the pygame window
 
+def play(g: PyDeepRTS):
+    pygame.init()
+
+    # Initial 2 players
+    player1 = g.players[0]
+    player2 = g.players[1]
+
+    # Start the game (flag)
+    g.start()
+
+    # Run forever
+    while True:
+        g.tick()  # Update the game clock
+        g.update()  # Process the game state
+        g.render()  # Draw the game state to graphics
+        g.caption()  # Show Window caption
+
+        g.view()  # View the game state in the pygame window
+
+        events_hack()
+
+        g.capture()
+
+        # If the game is in terminal state
+        if g.is_terminal():
+            g.stop()
+            print("Game over")
+            break
+
+        # Perform random action for player 1
+        player1.do_action(get_random_action())
+
+        # Perform random action for player 2
+        player2.do_action(get_random_action())
+
+
+def events_hack():
     # Hack to render the pygame
     events = pygame.event.get()
 
-    g.capture()
 
-    # If the game is in terminal state
-    if g.is_terminal():
-        g.reset()  # Reset the game
+def get_random_action():
+    return numpy.random.randint(0, 16)
 
-    # Perform random action for player 1
-    player1.do_action(numpy.random.randint(0, 16))
 
-    # Perform random action for player 2
-    player2.do_action(numpy.random.randint(0, 16))
+if __name__ == "__main__":
+    game = setup_game()
+    for _ in range(NUM_OF_GAMES):
+        play(game)
+        game.reset()
