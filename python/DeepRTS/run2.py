0f0e32cc9abb9e0fa74e0dce14db37457b12e99f
Dhineshkumar Ramasubbu <r.dhineshkumar2014@vit.ac.in>
2019-10-20 23:45:57 -0700

    run2.py for ann

--- python/DeepRTS/run2.py
+++ python/DeepRTS/run2.py
@@ -1,50 +1,75 @@
 import os
 
-import numpy
+import numpy as np
 import pygame
-
+from model.ANN import ANN
 from game.Game import Game
 
-MAP_NAME = '10x10-2v2.json'
+MAP_NAME = '10x10-ourgame.json'
 NUM_OF_GAMES = 3
 TRAIN = True
 SKIP_RATE = 10
 
 
-def play(g: Game):
+def play(g: Game, ann: ANN):
     # Initial 2 players
 
     player1 = g.get_players(1)[0]
     player2 = g.get_players(2)[0]
+    player3 = g.get_players(2)[1]
+    player4 = g.get_players(2)[2]
     player1.main_player = True
 
     # Start the game (flag)
     g.start()
     g.set_train(True)
+    g.update()
+    g.update_state()
+    g.prev_stat = g.get_state_stat()
+    # print(g.get_resource_matrix())
 
     while True:
         # If the game is in terminal state
-        if g.is_terminal():
+        if g.is_game_terminal() or g.get_ticks() > 3000:
+            file = open("./logs/evaluation.txt",'a+')
+            g.game_result(file)
+            file.close()
             g.stop()
             print("Game over")
             break
-
-        print("Player 1 : " + str(player1.location))
-
+        
+        # print("Player 1 : " + str(player1.location))
         # Player 1 action by model
         # TODO 599: Use the predicted action
-        # action = ddqn.predict_action(state)
-        player1.move_to(8, 8)
+        state = g.get_state()
+        predicted_action = ann.predict_action(state)
+        random_action = np.random.randint(2)
+        if(np.random.random() < max(0.01, np.power(ann.epsilon, ann.tick))):
+            action = random_action
+        else:
+            action = predicted_action
 
-        # Player 2 random action
-        player2.do_action(numpy.random.randint(1, 3))
-
+        #print("Player1 Action:", action)
+        player1.do_action(action)
+        # # # Player 2 random action
+        player2.do_action(2)
+        player3.do_action(2)
+        player4.do_action(2)
         update_with_skip_rate(g, SKIP_RATE)
         g.render()  # Draw the game state to graphics
         g.caption()  # Show Window caption
         g.update_state()  # Update states to new model
+        # print("Resource at ", i)
+        # # print(g.get_resource_matrix())
+        # print("Player Resource:", player1.player.getScore())
         g.view()  # View the game state in the pygame window
 
+        next_state = g.get_state()
+        reward = g.get_reward_value()
+
+        ann.train(state, action, reward, next_state, g.is_terminal())
+
+        state = next_state
 
 def update_with_skip_rate(g, skip_rate):
     skip_count = 0
@@ -52,7 +77,6 @@ def update_with_skip_rate(g, skip_rate):
         if g.update():
             if not TRAIN:
                 events_hack()
-            g.tick()
             skip_count += 1
         if skip_count == skip_rate:
             break
@@ -70,11 +94,19 @@ def get_random_action():
 if __name__ == "__main__":
     if TRAIN:
         os.environ["SDL_VIDEODRIVER"] = "dummy"
-        SKIP_RATE = 1
-
+        SKIP_RATE = 2
+    
+    file = open("./logs/evaluation.txt",'a+')
+    file.write("team_health,team_count,team_avg_health,team_resource,o_team_health,o_team_count,o_team_health_avg,o_team_resource,result,time_ticks")
+    file.close()
     game = Game(MAP_NAME, train=TRAIN)
-
-    for _ in range(45):
-        play(game)
+    game.add_a_player(2)
+    game.add_a_player(2)
+    ann = ANN()
+    print(ann.get_summary())
+    for _ in range(11):
+        play(game,ann)
         game.reset()
-    print(ddqn.get_summary())
+        if(_ % 10 == 0):
+            ann.save_model(str(int(_/10)))
+
