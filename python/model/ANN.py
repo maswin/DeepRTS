9bf29a41971381c29f81474003f6cb40f776520f
Dhineshkumar Ramasubbu <r.dhineshkumar2014@vit.ac.in>
2019-11-12 00:22:14 -0800

    Added Priority Experience Replay

--- python/model/ANN.py
+++ python/model/ANN.py
@@ -10,11 +10,13 @@ from keras.callbacks import TensorBoard
 import tensorflow as tf
 from time import time
 from keras.callbacks import History
+from collections import deque
 
 class ANN:
 
     def __init__ (self, load_model = False, load_weight = False, load_file = None):
-        self.memory = []
+        
+        memory_len = 10000
         self.tick = 1 
         self.learning_rate = 0.0001
         self.discount_factor = 0.99
@@ -22,11 +24,15 @@ class ANN:
         self.model = self.create_network(load_model, load_weight, load_file)
         self.target_model = self.create_network(False, False, False)
         self.epsilon = np.power(0.97, self.tick)
-        self.MAX_MEMORY_LENGTH = 10000
+        self.MAX_MEMORY_LENGTH = memory_len
         self.SAMPLE_SIZE = 32
+        self.memory = deque(maxlen = memory_len)
+        self.priorities = deque(maxlen = memory_len)
+        self.priority_scale = 1.0
+        self.priority_offset = 0.1
         self.replay_history = History()
         #self.tensorboard1 = TensorBoard(log_dir = "logs\log_inter_ann")
-        #self.tensorboard2 = TensorBoard(log_dir = "logs\log_replay_ann")
+        #self.tensorboard2 = TensorBoard(log_dir = "logs\log_replay_ann"
 
 
     def create_network(self, load_model = False, load_weight = False, load_file = None):
@@ -51,18 +57,57 @@ class ANN:
 
 
     def remember(self, state, action, reward, next_state, finished):
-        if(len(self.memory) < self.MAX_MEMORY_LENGTH):
-            self.memory.append((state, action, reward, next_state, finished))
-        else:
-            ind = np.random.randint(self.MAX_MEMORY_LENGTH)
-            self.memory[ind] = (state, action, reward, next_state, finished)
+        # if(len(self.memory) < self.MAX_MEMORY_LENGTH):
+        self.memory.append((state, action, reward, next_state, finished))
+        self.priorities.append(max(self.priorities, default=1))
+        # else:
+        #     ind = np.random.randint(self.MAX_MEMORY_LENGTH)
+        #     self.memory[ind] = (state, action, reward, next_state, finished)
+
+    def get_probabilities(self):
+        scaled_priorities = np.array(self.priorities) ** self.priority_scale
+        sample_probabilities = scaled_priorities / sum(scaled_priorities)
+        return sample_probabilities
+
+    def get_sample_weights(self, probabilities):
+        weights = 1/len(self.memory) * 1/probabilities
+        weights_normalized = weights / max(weights)
+        return weights_normalized
+
+    def set_priorities(self, indices, errors):
+        for i,e in zip(indices, errors):
+            self.priorities[i] = abs(e) + self.priority_offset
+
+    def replay_new_PER(self):
+        sample_size = min(len(self.memory), self.SAMPLE_SIZE)
+        sample_probs = self.get_probabilities()
+        sample_indices = random.choices(range(len(self.memory)), k=sample_size, weights=sample_probs)
+        minibatch = np.array(self.memory)[sample_indices]
+        sample_weights = (self.get_sample_weights(sample_probs[sample_indices])) ** (1-self.epsilon)
+
+        errors = []
+        for i in range(len(sample_indices)):
+            state, action, reward, next_state, finished = minibatch[i] 
+            end_result = reward  if finished is True else (
+                    reward + self.discount_factor * np.max(self.target_model.predict(next_state)[0]))
+            target = self.model.predict(state)
+            target[0][action] = end_result
+            self.model.fit(state, target, sample_weight = np.array([sample_weights[i]]), epochs=1, verbose=0, callbacks=[self.replay_history])
+            avg_loss = self.replay_history.history['loss'][0]
+            errors.append(avg_loss)
+        
+        self.set_priorities(sample_indices, errors)
+        
+        avg_loss = avg_loss/sample_size        
+        f = open("./logs_ann/model_metrics_replay.csv",'a+')
+        f.write(str(avg_loss)+ "\n")
+        f.close()
 
 
     def replay_new(self):
-        if(len(self.memory) <= self.SAMPLE_SIZE):
-            minibatch = self.memory
-        else:
-            minibatch = random.sample(self.memory, self.SAMPLE_SIZE)
+
+        sample_size = min(len(self.memory), self.SAMPLE_SIZE)
+        minibatch = random.sample(self.memory, sample_size)
         
         states = []
         targets = []
@@ -77,21 +122,19 @@ class ANN:
         states = np.array(states)
         targets = np.array(targets)
         self.model.fit(states, targets, epochs=1, verbose=0, callbacks=[self.replay_history])
-        avg_loss = self.replay_history.history['loss'][0]
+        loss = self.replay_history.history['loss'][0]
         
-        #avg_loss = avg_loss / self.SAMPLE_SIZE
         f = open("./logs_ann/model_metrics_replay.csv",'a+')
-        f.write(str(avg_loss)+ "\n")
+        f.write(str(loss)+ "\n")
         f.close()
 
     def immediate_update(self, state, action, reward, next_state, done):
-        if done:
-            target = reward
+        target = reward
         if not done:
             target = reward + self.discount_factor * np.amax(self.model.predict(next_state)[0])
         reward_pred = self.model.predict(state)
         reward_pred[0][action] = target
-        self.model.fit(state, reward_pred, epochs=1, verbose=0 )
+        self.model.fit(state, reward_pred, epochs=1, verbose=0)
 
     def transfer_weights(self):
         online_weights = self.model.get_weights()
@@ -104,7 +147,7 @@ class ANN:
     def train(self, state, action, reward, next_state, done):
         self.remember(state, action, reward, next_state, done)
         #self.immediate_update(state, action, reward, next_state, done)
-        self.replay_new()
+        self.replay_new_PER()
         # if(self.tick % 100 == 0):
         #     self.replay_new()
 
